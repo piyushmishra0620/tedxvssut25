1370918aa853fe3081a6cb1dd583137f453336e1
iddev5 <ayushbardhan5@gmail.com>
2025-10-03 13:18:46 +0530

    Edit and merge header component
    Co-Authored-By: Saran Rath <rathsaranner@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,30 @@
+import './header.css';
 
-export const Header = () => {
+const Header = () => {
 	return (
-		<p className="font-helvetica font-bold">Header</p>
+	<header className="navbar-container">
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <a href="#home">
+                    <span className="logo-ted">TED</span><span className="logo-x">x</span><span className="logo-vssut">VSSUT</span>
+                </a>
+            </div>
+
+            <ul className="nav-links">
+                <li><a href="#home" className="active">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#speakers">Speakers</a></li>
+                <li><a href="#sponsors">Sponsors</a></li>
+                <li><a href="#crew">Crew</a></li>
+                <li><a href="#pastevents">Past Events</a></li>
+            </ul>
+
+            <a href="#contact" className="contact-btn-link">
+                <button className="contact-btn">Contact Us</button>
+            </a>
+        </nav>
+    </header>
 	);
 }
+
+export default Header;
